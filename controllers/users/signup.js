246164ce8b617fe8008937bff1f498f517d670d6
asus <natakulibaba88@gmail.com>
2022-06-16 10:07:36 +0300

    finish

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,5 +1,6 @@
 const { User } = require("../../models");
 const { Conflict } = require("http-errors");
+const gravatar = require('gravatar')
 const bcrypt = require("bcryptjs");
 
 const signup = async (req, res) => {
@@ -8,12 +9,16 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict(`Email ${email} in use`);
   }
+
+  const avatarURL = gravatar.url(email,{},true);
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  await User.create({ email, password: hashPassword });
-  res.status(201).json({
+  await User.create({ email, password: hashPassword,avatarURL });
+  return res.status(201).json({
+    code:201,
     user: {
       email,
       subscription: "starter",
+      avatarURL,
     },
   });
 };
